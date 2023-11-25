dd22b00893013ba71418b2155b865e30a66ab3a5
sergious234 <sergious234@gmail.com>
2023-11-25 13:55:38 +0100

    Plots and speed up

--- practica5/main.py
+++ practica5/main.py
@@ -1,10 +1,15 @@
-from email.policy import default
-import time
-from matplotlib import axis
-import pandas as pd
-import numpy as np
-import random
 import logging
+import random
+import time
+
+# Librerias necesarias:
+#   matplotlib  (pip install matplotlib)
+#   numpy       (pip install numpy)
+#   pandas      (pip install pandas)
+#   sklearn     (pip install scikit-learn)
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from sklearn.metrics import davies_bouldin_score
 
 N_CLUSTERS = 2
@@ -13,19 +18,30 @@ CORTE = 0.01
 COLUMS = [1, 3, 4]
 
 
+#  ██████╗ ██╗   ██╗███╗   ██╗████████╗ ██████╗      ██╗
+#  ██╔══██╗██║   ██║████╗  ██║╚══██╔══╝██╔═══██╗    ███║
+#  ██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║   ██║    ╚██║
+#  ██╔═══╝ ██║   ██║██║╚██╗██║   ██║   ██║   ██║     ██║
+#  ██║     ╚██████╔╝██║ ╚████║   ██║   ╚██████╔╝     ██║
+#  ╚═╝      ╚═════╝ ╚═╝  ╚═══╝   ╚═╝    ╚═════╝      ╚═╝
+#
 class KMeans:
 
-    distance = "euclidean"
+    def __init__(self, df, colums, k=N_CLUSTERS, max_it=NUMERO_IT, corte=CORTE, distance="euclidean") -> None:
+        self.__start = None
+        self.__end = None
+        self.minimos = None
+        self.it = None
 
-    def __init__(self, df, colums, k=N_CLUSTERS, max_it=NUMERO_IT, corte=CORTE) -> None:
         self.colums = colums
         self.max_it = max_it
         self.corte = corte
         self.k = k
+        self.distance = distance
 
         # Crear las listas vacias de centroides
         # y la lista de clusters vacios
-        self.centroids: list[tuple[float, ...]] = [(0.0,)*k for _ in range(k)]
+        self.centroids: list[tuple[float, ...]] = [(0.0,) * k for _ in range(k)]
         self.clusters = [[] for _ in range(k)]
 
         # Obtener las columnas de interes
@@ -43,25 +59,28 @@ class KMeans:
         for cluster in self.clusters:
             cluster.clear()
 
-        logging.info(f"Initial centroids: {self.centroids}")
-        logging.info(f"Initial cluster: {self.clusters} ")
-        print("\n\n")
+        # logging.info(f"Initial centroids: {self.centroids}")
+        # logging.info(f"Initial cluster: {self.clusters}\n\n")
 
     def compute(self):
         condition = True
         self.it = 0
-        self.start = time.time()
+        self.__start = time.time()
+        examples = np.array(self.df.values)
+        filas, _ = examples.shape
+        distancias = np.zeros((filas, self.k))
         while condition:
-            print("\n\n\n")
-            logging.info(f"It {self.it}")
+            logging.info(f"It {self.it}\n\n\n")
 
             # Calcular la primera columna de distancias euclidea con el centroide 0
-            distancias = self.__compute_distance(self.df.values, self.centroids[0])
+            distancias[:, 0] = self.__compute_distance(
+                examples, self.centroids[0])
 
             # Ir añadiendo las columnas de los otros centroides
-            for centroid in self.centroids[1:]:
-                distancias_i = self.__compute_distance(self.df.values, centroid)
-                distancias = np.column_stack([distancias, distancias_i])
+            for i, centroid in enumerate(self.centroids[1:]):
+                distancias[:, i + 1] = self.__compute_distance(
+                    examples, centroid)
+                # distancias = np.column_stack([distancias, distancias_i])
 
             # De cada fila, obtener el indice del minimo
             # Guardarlo en la clase para despues calcular el
@@ -84,19 +103,19 @@ class KMeans:
 
             # Como ya sabemos de cual cluster esta mas cerca cada ejemplo
             # con lo obtenido en minimos, añadir cada ejemplo a su cluster
-            for i, example in enumerate(self.df.values):
+            for i, example in enumerate(examples):
                 self.clusters[self.minimos[i]].append(example)
 
             # Obtener los nuevos centroides
             new_centroids = self.__compute_centroids()
-            logging.info(f"Old centroids: {self.centroids}")
-            logging.info(f"New centroids: {new_centroids}")
-            logging.info(f"Clusters: {[len(x) for x in self.clusters]}")
+            # logging.info(f"Old centroids: {self.centroids}")
+            # logging.info(f"New centroids: {new_centroids}")
+            # logging.info(f"Clusters: {[len(x) for x in self.clusters]}")
 
             # Obtener la diferencia de los centroides antiguos con los nuevos
-            diff = np.square(np.array(self.centroids) -
-                             np.array(new_centroids))
-            logging.info(f"Diff  : {diff}")
+            diff = np.square(np.array(self.centroids) - np.array(new_centroids))
+
+            # logging.info(f"Diff  : {diff}")
 
             # Mirar si todas las diferencias son menores que el CORTE, en caso
             # de que todos los centroides hayan variado menos que CORTE => parada = True
@@ -111,64 +130,149 @@ class KMeans:
                 for cluster in self.clusters:
                     cluster.clear()
 
-        self.end = time.time()
+        self.__end = time.time()
         return
 
     # See: https://en.wikipedia.org/wiki/Davies%E2%80%93Bouldin_index
-    def davies_bouldin_index(self, X, labels):
+    def davies_bouldin_index(self):
+        x = self.df.values
+        labels = self.minimos
         # Convertir los centroides a un array de NumPy
         centroids = np.array(self.centroids)
         # Inicializar una matriz para almacenar las medidas de dispersión dentro de cada clúster
         dispersion = np.zeros(self.k)
         for i in range(self.k):
             # Calcular la dispersión dentro de cada clúster
-            dispersion[i] = np.sum(np.linalg.norm(X[labels == i] - centroids[i], axis=1)) / len(X[labels == i])
+            dispersion[i] = np.sum(np.linalg.norm(
+                x[labels == i] - centroids[i], axis=1)) / len(x[labels == i])
 
-        centroid_distances = np.linalg.norm(centroids[:, np.newaxis, :] - centroids[np.newaxis, :, :], axis=2)
+        centroid_distances = np.linalg.norm(
+            centroids[:, np.newaxis, :] - centroids[np.newaxis, :, :], axis=2)
         dispersions = dispersion + dispersion[:, np.newaxis]
-        dispersions = dispersions * (np.ones(dispersions.shape) - np.eye(dispersions.shape[0]) )
-        D = dispersions/(centroid_distances + np.eye(*centroid_distances.shape))
+        dispersions = dispersions * (np.ones(dispersions.shape) - np.eye(dispersions.shape[0]))
+        d = dispersions / (centroid_distances + np.eye(*centroid_distances.shape))
 
-        return np.sum(np.max(D, axis=1)) / self.k
+        return np.sum(np.max(d, axis=1)) / self.k
+
+    def inertia(self) -> np.float64:
+        dispersion = np.zeros(self.k)
+        for i in range(self.k):
+            # Calcular la dispersión dentro de cada clúster
+            dispersion[i] = np.sum(np.square(
+                self.df.values[self.minimos == i] - self.centroids[i]))
+
+        return np.sum(dispersion)
+
+    def time(self) -> float:
+        return self.__end - self.__start
 
     def __compute_distance(self, examples, centroid) -> np.ndarray:
         match self.distance:
-            case "euclidean": return np.linalg.norm(examples - centroid, axis=1)
-            case "manhattan": return np.sum(np.abs(examples - centroid), axis=1)
+            case "euclidean":
+                return np.linalg.norm(examples - centroid, axis=1)
+            case "manhattan":
+                return np.sum(np.abs(examples - centroid), axis=1)
             # Caso por defecto devolver distancia euclidea
-            case _: return np.linalg.norm(examples - centroid, axis=1)
+            case _:
+                return np.linalg.norm(examples - centroid, axis=1)
 
     def __compute_centroids(self) -> list[tuple[float, ...]]:
         centroids = []
 
-        for cluster in self.clusters:
-            values = [0.0 for _ in range(len(self.colums))]
-            for example in cluster:
-                values += example
-            values = [x/len(cluster) for x in values]
-            centroids.append(values)
+        for i, cluster in enumerate(self.clusters):
+            if len(cluster) == 0:
+                logging.error(f"Cluster vacio k={self.k}, it={self.it}")
+                logging.warning(f"No se calculará un nuevo centroide, se usará el antiguo")
+                centroids.append(self.centroids[i])
+            else:
+                s = np.sum(cluster, axis=0)
+                new_centroid = s / len(cluster)
+                centroids.append(new_centroid)
         return centroids
 
     def __str__(self) -> str:
-        return f"\n\n\n"+\
-            f"RESULTADO\n" +\
-            f"N ITs: {self.it+1}\n" +\
-            f"Centroides: {self.centroids}\n" +\
-            f"Clusters elemens: {[len(x) for x in self.clusters]}\n" +\
-            f"N CLUSTERS: {len(self.clusters)}\n" +\
-            f"DB indice: {self.davies_bouldin_index(self.df.values, self.minimos)}\n" +\
-            f"SKDB indice: {davies_bouldin_score(self.df.values, self.minimos)}\n" +\
-            f"Tiempo transcurrido: {self.end-self.start} segundos\n" 
+        return f"\n\n\n" + \
+            f"RESULTADO\n" + \
+            f"N ITs: {self.it + 1}\n" + \
+            f"Centroides: {self.centroids}\n" + \
+            f"Clusters elemens: {[len(x) for x in self.clusters]}\n" + \
+            f"N CLUSTERS: {len(self.clusters)}\n" + \
+            f"DB   indice: {self.davies_bouldin_index()}\n" + \
+            f"SKDB indice: {davies_bouldin_score(self.df.values, self.minimos)}\n" + \
+            f"Tiempo transcurrido: {self.time()} segundos\n"
 
 
 def main():
+    results = []
     df = pd.read_csv("analisis.csv")
-    kmeans = KMeans(df, colums=COLUMS, k=2, max_it=10, corte=0.01)
+
+    #  ██████╗ ██╗   ██╗███╗   ██╗████████╗ ██████╗     ██████╗
+    #  ██╔══██╗██║   ██║████╗  ██║╚══██╔══╝██╔═══██╗    ╚════██╗
+    #  ██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║   ██║     █████╔╝
+    #  ██╔═══╝ ██║   ██║██║╚██╗██║   ██║   ██║   ██║    ██╔═══╝
+    #  ██║     ╚██████╔╝██║ ╚████║   ██║   ╚██████╔╝    ███████╗
+    #  ╚═╝      ╚═════╝ ╚═╝  ╚═══╝   ╚═╝    ╚═════╝     ╚══════╝
+    #
+    results_df = pd.DataFrame(columns=["K", "Daivis-Bouldin"])
+    for k in range(2, 60):
+        kmeans = KMeans(df, colums=COLUMS, k=k, max_it=20, corte=0.01, distance="euclidean")
+        kmeans.compute()
+        results.append((k, kmeans.inertia()))
+        results_df.loc[len(results_df), :] = [k, kmeans.davies_bouldin_index()]
+
+    print(results_df.to_string(index=False))
+
+    #  ██████╗ ██╗   ██╗███╗   ██╗████████╗ ██████╗     ██████╗
+    #  ██╔══██╗██║   ██║████╗  ██║╚══██╔══╝██╔═══██╗    ╚════██╗
+    #  ██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║   ██║     █████╔╝
+    #  ██╔═══╝ ██║   ██║██║╚██╗██║   ██║   ██║   ██║     ╚═══██╗
+    #  ██║     ╚██████╔╝██║ ╚████║   ██║   ╚██████╔╝    ██████╔╝
+    #  ╚═╝      ╚═════╝ ╚═╝  ╚═══╝   ╚═╝    ╚═════╝     ╚═════╝
+
+    df_ordenado = results_df.sort_values(by="Daivis-Bouldin", ascending=False)
+    print("\n\nLos 5 mejores K segun el Davies-Bouldin index son: ")
+    print(df_ordenado[:5].to_string(index=False))
+
+    x, y = zip(*results)
+    plt.plot(x, y, marker='o', linestyle='-')
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    #  ██████╗ ██╗   ██╗███╗   ██╗████████╗ ██████╗     ██╗  ██╗
+    #  ██╔══██╗██║   ██║████╗  ██║╚══██╔══╝██╔═══██╗    ██║  ██║
+    #  ██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║   ██║    ███████║
+    #  ██╔═══╝ ██║   ██║██║╚██╗██║   ██║   ██║   ██║    ╚════██║
+    #  ██║     ╚██████╔╝██║ ╚████║   ██║   ╚██████╔╝         ██║
+    #  ╚═╝      ╚═════╝ ╚═╝  ╚═══╝   ╚═╝    ╚═════╝          ╚═╝
+    #
+
+    kmeans = KMeans(df, colums=COLUMS, k=5, max_it=20, corte=0.01, distance="euclidean")
     kmeans.compute()
-    print(kmeans)
+
+    colores = ["red", "green", "blue", "yellow", "pink"]
+    for i, clase in enumerate(set(kmeans.minimos)):
+        values = kmeans.df.values[kmeans.minimos == clase, :]
+        ax.scatter(values[:, 0], values[:, 1], values[:, 2], c=colores[i], label=f"Clase {i}")
+
+    for i, centroid in enumerate(kmeans.centroids):
+        ax.scatter(*centroid, c=colores[i], marker='*', s=200, label=f"Centroide clase {i}")
+
+    ax.set_xlabel(df.columns[kmeans.colums[0]])
+    ax.set_ylabel(df.columns[kmeans.colums[1]])
+    ax.set_zlabel(df.columns[kmeans.colums[2]])
+    ax.set_title(f'Clusters = {kmeans.k}')
+
+    # La leyenda suele salir un poco donde le da la gana
+    # Aumentar el tamaño de la ventana para que se pueda ver
+    # bien y no aparezca en medio del grafico
+    ax.legend(loc="lower right", fontsize=12, bbox_to_anchor=(-0.2, 0.0))
+
+    plt.show()
 
 
 if __name__ == "__main__":
     logging.basicConfig(
-        format="[%(levelname)s]: %(message)s", level=logging.INFO)
+        format="[%(levelname)s]: %(message)s", level=logging.WARN)
     main()
